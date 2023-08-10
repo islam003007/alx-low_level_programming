a32055646d32a2a442b5786d878cf4bfd9b922c5
islam <islam003007@gmail.com>
2023-08-10 16:48:32 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*a program that prints a simple line*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
