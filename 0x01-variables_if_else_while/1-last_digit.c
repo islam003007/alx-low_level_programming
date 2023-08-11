235c02a4837d3b1f7e966db082d6454066541605
islam <islam003007@gmail.com>
2023-08-12 02:45:39 +0300

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	else if (last_digit == 0)
 		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	else
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, 
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n,
 		last_digit);
 	return (0);
 }
