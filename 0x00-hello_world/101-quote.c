e48d55d0e7ad873d0b6f917f637f8f46d54adb41
islam <islam003007@gmail.com>
2023-08-10 18:10:51 -0700

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * description
+ *
+ * Return: 1
+ *
+ *
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
