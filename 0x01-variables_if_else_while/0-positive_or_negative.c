d1ac1542368b4918e62cfd207502207ea48307dc
islam <islam003007@gmail.com>
2023-08-12 02:11:40 +0300

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-		puts("negative");
+	if (n == 0)
+		puts("is zero");
+	else if (n < 0)
+		puts("is negative");
 	else
-		puts("positive");
+		puts("is positive");
 	return (0);
 }
