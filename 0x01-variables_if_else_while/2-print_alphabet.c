ffc4f5d3408d1e71695f6fe77ba221a75ea1c7b4
islam <islam003007@gmail.com>
2023-08-12 03:05:03 +0300

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: postive or negative number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	puts("");
+}
