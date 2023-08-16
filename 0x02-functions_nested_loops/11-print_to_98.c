040cadd2a424d4b44863a765f1c44fea7a336c94
islam <islam003007@gmail.com>
2023-08-16 20:24:31 +0300

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,61 +11,17 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
-		{
-			if (i > 99)
-			{
-				_putchar(i / 100 + '0');
-				_putchar((i / 10) % 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-		}
+        {
+            printf("%d, ", i);
+        }
 	}
 	else
 	{
-		int flag;
-
-		flag = 0;
-		for (i = n; i < 98; i++)
-		{
-			if (i < 0)
-			{
-				i = i * -1;
-				_putchar('-');
-				flag = 1;
-			}
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else if (i < 100)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			else
-			{
-				_putchar(i / 100 + '0');
-				_putchar((i / 10) % 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-			if (flag)
-			{
-				i = i * -1;
-				flag = 0;
-			}
-		}
+        for (i = n; i < 98; i++)
+        {
+            printf("%d, ", i);
+        }
 	}
-	 _putchar('9');
-	 _putchar('8');
-	 _putchar('\n');
+	printf("98\n");
 
 }
