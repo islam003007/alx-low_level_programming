bf1a0428588deed41ec3479cc040ab0f07dc9282
islam <islam003007@gmail.com>
2023-08-16 22:01:56 +0300

    new

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	unsigned long i, fib1, fib2, sum;
+
+    fib1 = 0;
+    fib2 = 1;
+    printf("%lu ,%lu", fib1, fib2);
+	for (i = 0; i < 48; i++)
+	{
+        sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = sum;
+		printf(", %lu", fib2);
+	}
+    printf("\n");
+	return (0);
+}
