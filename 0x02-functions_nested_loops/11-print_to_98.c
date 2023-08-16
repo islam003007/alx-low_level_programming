6e7e6aa173412bf86f339dd07d5b90516cdcc3f1
islam <islam003007@gmail.com>
2023-08-16 20:11:42 +0300

    new

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,65 @@
+#include "main.h"
+/**
+ * print_to_98 - print all natural numbers leading to 98
+ * 
+ * @n: reads input
+*/
+void print_to_98(int n)
+{
+    int i;
+
+    if (n > 98)
+    {
+        for (i = n; i > 98; i--)
+        {
+            if (i > 99)
+            {
+                _putchar(i / 100 + '0');
+                _putchar((i / 10) % 10 + '0');
+                _putchar(i % 10 + '0');
+            }
+            else
+            {
+                _putchar(i / 10 + '0');
+                _putchar(i % 10 + '0');
+            }
+            _putchar(',');
+            _putchar(' ');
+        }   
+    }
+    else
+    {
+        int flag;
+
+        flag = 0;
+        for (i = n; i < 98; i++)
+        {
+            if (i < 0)
+            {
+                i = i * -1;
+                _putchar('-');
+                flag = 1;
+            }
+            if (i < 10)
+            {
+                _putchar(i + '0');
+            }
+            else
+            {
+                _putchar(i / 10 + '0');
+                _putchar(i % 10 + '0');
+            }
+            _putchar(',');
+            _putchar(' ');
+            if (flag)
+            {
+                i = i * -1;
+                flag = 0;
+            }
+        }
+    }
+     _putchar('9');
+     _putchar('8');
+     _putchar('\n');
+
+}
