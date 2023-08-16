a15e6f430b0524f3f14b01cb59955cf9bc0ef612
islam <islam003007@gmail.com>
2023-08-16 17:45:01 +0300

    new

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,36 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day
- * 
- * 
+ *
+ *
 */
 
 void jack_bauer(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 3; i++)
-    {
-        int j;
+	for (i = 0; i < 3; i++)
+	{
+		int j;
 
-        for (j = 0; j < 4; j++)
-        {
-            int k;
+		for (j = 0; j < 4; j++)
+		{
+			int k;
 
-            for (k = 0; k < 6; k++)
-            {
-                int l;
+			for (k = 0; k < 6; k++)
+			{
+				int l;
 
-                for (l = =; l < 10; l++)
-                {
-                    _putchar(i);
-                    _putchar(j);
-                    _putchar(':');
-                    _putchar(k);
-                    _putchar(l);
-                    _putchar('\n');
-                }
-            }
-        }
-    }
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
