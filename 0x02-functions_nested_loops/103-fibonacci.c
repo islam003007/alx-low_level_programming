8343c4ce93649c40195d8c3c07f89a6de6493f86
islam <islam003007@gmail.com>
2023-08-16 22:25:38 +0300

    new

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,12 +13,13 @@ int main(void)
 	fib2 = 2;
 	sum2 = 0;
 	printf("%lu", fib1);
-	for (i = 0; i < 300; i++)
+	while (1)
 	{
 		sum = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = sum;
-		sum2 += fib1;
+		if (fib1 % 2 ==0)
+			sum2 += fib1;
 		if (fib1 >= 4000000)
 			break;
 		printf("%lu", fib2);
