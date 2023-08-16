6957fecfaa441926a9992598fed8081a958d319c
islam <islam003007@gmail.com>
2023-08-16 11:41:45 +0300

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - checks sign of a number
+ *
+ * @n: checks the input
+ *
+ * Return: 1 (positive) , 0 (zero) , -1 (negative)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
