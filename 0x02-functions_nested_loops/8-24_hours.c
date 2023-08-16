a452e57f1149c232cedaf6ad3af0c5a061bdb93c
islam <islam003007@gmail.com>
2023-08-16 17:56:10 +0300

    new

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,27 +9,22 @@ void jack_bauer(void)
 {
 	int i;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 24; i++)
 	{
-		int j;
+		int k;
 
-		for (j = 0; j < 4; j++)
+		for (k = 0; k < 6; k++)
 		{
-			int k;
+			int l;
 
-			for (k = 0; k < 6; k++)
+			for (l = 0; l < 10; l++)
 			{
-				int l;
-
-				for (l = 0; l < 10; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
 			}
 		}
 	}
