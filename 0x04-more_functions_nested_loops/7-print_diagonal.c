741a0e1e3708d49b566700f85aef91f2a5c3a82a
islam <islam003007@gmail.com>
2023-08-17 12:59:11 +0300

    new

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,18 @@ void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		int j;
-
-		for (j = 0; j < i; j++)
-			_putchar(' ');
-		_putchar('\\');
+	if (n <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
