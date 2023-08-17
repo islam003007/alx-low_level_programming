0afaffcde285a14bd461b6f0bfdca3cb9c8b792c
islam <islam003007@gmail.com>
2023-08-17 10:09:44 +0300

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - checks if character is uppercase
+ *
+ * @c: reads input
+ *
+ * Return: 1 (uppercase), 0 (otherwise)
+ **/
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+}
