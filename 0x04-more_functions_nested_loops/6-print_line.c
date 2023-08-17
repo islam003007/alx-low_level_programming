1e559c6016c2c7cd4169dcd5813d56640ac891bb
islam <islam003007@gmail.com>
2023-08-17 11:00:23 +0300

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @n: inputs the length of the line
+ *
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+}
