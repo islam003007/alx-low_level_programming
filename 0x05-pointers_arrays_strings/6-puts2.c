bef89c69fecb3cf8e3a1e2c5d97463cde118525f
islam <islam003007@gmail.com>
2023-08-21 22:52:20 +0300

    new

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,15 @@
 */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i])
+		i++;
+	while (i >= 0)
 	{
 		_putchar(*str);
 		str += 2;
+		i -= 2;
 	}
 	_putchar('\n');
 }
