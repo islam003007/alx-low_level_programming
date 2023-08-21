4e907f5c7b4b6363687ff84e390a8883cd616037
islam <islam003007@gmail.com>
2023-08-21 21:36:25 +0300

    new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev: reverses a string and prints it to the console
+ *
+ * @s: provides input string
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
