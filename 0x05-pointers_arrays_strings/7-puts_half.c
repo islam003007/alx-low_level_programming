6a021c35ce7b34a949d70e9e03b09f48ffa3bbfa
islam <islam003007@gmail.com>
2023-08-21 23:06:09 +0300

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints a the second half of a string followed by a new line
+ *
+ *
+ * @str: provides input string
+ *
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i])
+	{
+		i++;
+	}
+	j = i / 2;
+	while (str[j])
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
