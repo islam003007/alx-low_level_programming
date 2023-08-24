a573330a4f0c0dfa4de64c2ca239f4b011e4ae0a
islam <islam003007@gmail.com>
2023-08-24 09:11:26 +0300

    new

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string into another string
+ *
+ * @dest: provides destination.
+ * @src: provides source.
+ * @n: provides max charachters to be copied
+ *
+ * Return: a pointer to destination
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	while (src[index] && index < n)
+	{
+		dest[index] = src[index];
+	    index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
