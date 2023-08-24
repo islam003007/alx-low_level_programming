917049d34e6cf91663478a45bd0abc0ab455ed8e
islam <islam003007@gmail.com>
2023-08-24 08:43:51 +0300

    new

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: provides destination
+ * @src: provides source
+ *
+ * Return: a pointer to destination
+*/
+char *_strcat(char *dest, char *src)
+{
+	int dest_index = 0;
+	int src_index = 0;
+
+	while (dest[dest_index])
+		dest_index++;
+	while (src[src_index])
+	{
+		dest[dest_index] = src[src_index];
+		src_index++;
+		dest_index++;
+	}
+	dest[dest_index] = src[src_index];
+	return (dest);
+}
