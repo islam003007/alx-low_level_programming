5ae1d98e2261d9d0458b7c8aef84f95b40daa252
islam <islam003007@gmail.com>
2023-08-24 20:48:58 +0300

    new

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lower case litters of a string to upper case.
+ * 
+ * @a: provides input string.
+ * 
+ * Return: char pointer of result.
+*/
+char *string_toupper(char *a)
+{
+    int i = 0;
+
+    while (a[i])
+    {
+        if (a[i] >= 97 && a[i] <= 122)
+        {
+            a[i] -= 32; 
+        }
+        i++;
+    }
+    return (a);
+}
