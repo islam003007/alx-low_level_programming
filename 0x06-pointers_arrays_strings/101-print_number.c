40e3d0fd46133185910f7dee7d3cc4fcf6b26096
islam <islam003007@gmail.com>
2023-08-24 23:58:17 +0300

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,15 @@
 */
 void print_number(int n)
 {
-
+	unsigned int temp;
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
+	temp = n;
+	if (temp / 10)
+		print_number(temp / 10);
 
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar(n % 10 + '0');
+	_putchar(temp % 10 + '0');
 }
