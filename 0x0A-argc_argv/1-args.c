c666781a0922c8c03bf10236155e05ac9060f081
islam <islam003007@gmail.com>
2023-09-01 21:51:25 +0300

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: argument.
+ * Return: always 0 (success).
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
