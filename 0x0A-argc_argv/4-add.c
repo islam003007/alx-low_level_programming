2b81aa9e0bc09f9c904e261f30febf422ae1a1e4
islam <islam003007@gmail.com>
2023-09-01 22:30:52 +0300

    new

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: argument.
+ * Return: always 0 (success).
+*/
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	while (--argc)
+	{
+		if (!atoi(*(++argv)))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(*argv);
+	}
+	printf("%d\n", result);
+	return (0);
+}
