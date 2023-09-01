cad8a272d2818c07bf7ed3dd199a0db0ab054b4e
islam <islam003007@gmail.com>
2023-09-01 22:14:12 +0300

    new

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: argument.
+ * Return: always 0 (success).
+*/
+
+int main(int argc, char *argv[])
+{
+	int result = 1;
+
+	if (argc > 1)
+	{
+		while (--argc)
+		{
+			result *= atoi(*(++argv));
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
