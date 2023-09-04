d847b3b509bf688205ee5820be2fbe2e80bf9176
islam <islam003007@gmail.com>
2023-09-05 02:03:55 +0300

    new

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - allocates space in memory and copies string into it.
+ *
+ * @str: inputs string
+ *
+ * Return: pointer to the string.
+*/
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+	int j;
+
+	while (str[i++])
+	;
+
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		s[j] = str[j];
+
+
+
+
+
+}
