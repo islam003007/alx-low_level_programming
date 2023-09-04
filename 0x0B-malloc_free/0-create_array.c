41acfcee8ab003bb068681ce88e978d9cc2e5f8b
islam <islam003007@gmail.com>
2023-09-05 01:38:45 +0300

    new

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,24 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc(sizeof(char) * size);
 	unsigned int i;
+	char *arr;
 
 	if (size <= 0)
+	{
 		arr = NULL;
+		return (arr);
+	}
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
+
 	return (arr);
 }
