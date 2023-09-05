36d11a38f32e94528a7dacd9b224bd707b1ecdad
islam <islam003007@gmail.com>
2023-09-05 03:58:31 +0300

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,16 +22,19 @@ int **alloc_grid(int width, int height)
 		free(grid);
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
 		{
-			for (i = i; i >= 0; i--)
+			for (i--; i >= 0; i--)
 				free(grid[i]);
+
 			return (NULL);
 		}
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
