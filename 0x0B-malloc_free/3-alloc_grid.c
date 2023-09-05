aa35e6d1f95ae98e62b9891585a13f654e7bf131
islam <islam003007@gmail.com>
2023-09-05 03:48:08 +0300

    new

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * alloc_grid - dynamically allocates a 2-d array.
+ *
+ * @width: inputs width.
+ * @height: inputs height
+ *
+ * Return: pointer to 2-d array.
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
