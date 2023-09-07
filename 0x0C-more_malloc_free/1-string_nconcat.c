3f6882c4859460de7b6570396ac0772832d05de2
islam <islam003007@gmail.com>
2023-09-07 13:44:01 +0300

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (n < 0)
+		return (NULL);
 
 	for (s1_length = 0; s1[s1_length]; s1_length++)
 		;
