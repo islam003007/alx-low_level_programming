da4dd158deffa00804f0de91efc8fe6ca1f1dcad
islam <islam003007@gmail.com>
2023-09-08 14:31:57 +0300

    new

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,11 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long int result;
+	unsigned long int mul;
+	int num1, num2;
 
-	(void)argc;
-	(void)argv;
-	(void)result;
-	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[2]);
+	num2 = atoi(argv[3]);
+	mul = num1 * num2;
+
+	printf("%lu", mul);
+
 	return (0);
 }
