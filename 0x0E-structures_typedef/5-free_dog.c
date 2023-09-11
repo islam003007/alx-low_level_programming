f8770145098a664a623dff7cfba5184856869f2f
islam <islam003007@gmail.com>
2023-09-12 01:35:30 +0300

    new

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory of struct dog.
+ *
+ * @d: inputs variable.
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
