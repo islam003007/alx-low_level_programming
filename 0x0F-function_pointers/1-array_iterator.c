748b328a10dab0e3635eec59b3902159e666efd4
islam <islam003007@gmail.com>
2023-09-13 14:58:39 +0300

    new

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,14 +3,14 @@
 /**
  * array_iterator - excutes a function on each element of an array.
  *
- * @arry: inputs array.
+ * @array: inputs array.
  * @size: inputs array size.
  * @action: inputs pointer to action fucntion.
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (array && action)
 	{
