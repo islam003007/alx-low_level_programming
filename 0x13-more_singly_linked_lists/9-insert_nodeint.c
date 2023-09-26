0036f093c827e2d6ebd65ca6893d8ac3ca400326
Islam Rafat <islam003007@gmail.com>
2023-09-26 06:35:50 +0300

    new

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,16 +37,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	new->n = n;
+
 	if (idx != 0)
 	{
-		new->n = n;
 		new->next = temp->next;
 		temp->next = new;
 	}
 	else
 	{
-		new->n = n;
-		new->next = NULL;
+		new->next = *head;
+		*head = new;
 	}
 	return (new);
 }
