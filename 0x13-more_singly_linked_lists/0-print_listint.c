5aaa1a9465a2ea0671731921bca5417d9ffdee84
Islam Rafat <islam003007@gmail.com>
2023-09-26 04:24:50 +0300

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,15 +2,15 @@
 
 /**
  * print_listint - prints all elements of a listint_t.
- * 
+ *
  * @h: inputs header.
- * 
+ *
  * Return: number of elements in list.
 */
 
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 	const listint_t *temp = h;
 
 	while (temp != NULL)
