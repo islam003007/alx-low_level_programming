4903b98ed7c1d83494df0a146c18dd4b459277f6
Islam Rafat <islam003007@gmail.com>
2023-09-26 05:34:24 +0300

    new

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,12 +2,12 @@
 
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t
+ * get_nodeint_at_index - returns the nth node of a listint_t.
  *
  * @head: inputs head.
  * @index: inputs index.
  *
- * Returns: node to nth node or NULL if it doesn't exist.
+ * Return: node to nth node or NULL if it doesn't exist.
 */
 
 
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		if (head == NULL)
-			(return (NULL));
+			return (NULL);
 
 		head = head->next;
 	}
