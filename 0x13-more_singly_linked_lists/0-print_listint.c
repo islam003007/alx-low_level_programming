b8ed773f84d21e6c67491430ebeca1dea05d59f5
Islam Rafat <islam003007@gmail.com>
2023-09-26 04:24:28 +0300

    new

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t.
+ * 
+ * @h: inputs header.
+ * 
+ * Return: number of elements in list.
+*/
+
+size_t print_listint(const listint_t *h)
+{
+    size_t i = 0;
+	const listint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%i\n", temp->n);
+
+		temp = temp->next;
+		i++;
+	}
+
+	return (i);
+}
