140f9368867fd4db962384001398bf60b0108984
Islam Rafat <islam003007@gmail.com>
2023-11-06 00:43:26 +0200

    new

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ *
+ * @n: inputs number.
+ *
+ * Return: binary representation of a number.
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i, flag = 0;
+	unsigned long int temp;
+
+	for (i = 63; i >= 0; i--)
+	{
+		temp = n >> i;
+
+		if (temp & 1)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+	}
+
+	if (!flag)
+		_putchar('0');
+}
