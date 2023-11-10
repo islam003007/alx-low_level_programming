b1b1ff68c069c33eff8117fa0b83912f8813c6d0
Islam Rafat <islam003007@gmail.com>
2023-11-10 21:03:11 +0200

    new

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file then prints it.
+ *
+ * @filename: inputs file pathname.
+ * @letters: number of letters to read or write.
+ *
+ * Return: the actual read characters.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp, i;
+	char buffer[9999];
+
+	fp = open(filename, O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	if (read(fp, buffer, letters) == 0)
+		return (0);
+
+	i = write(1, buffer, letters);
+
+	if (i == -1)
+		return (0);
+
+	return (i);
+
+}
