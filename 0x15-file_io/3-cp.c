f7d288f774958c00e263a2b83330f884b2022e01
Islam Rafat <islam003007@gmail.com>
2023-11-11 01:00:38 +0200

    new

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,30 +19,22 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from < 0)
 	{
 		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to < 0)
-	{
-		exit(99);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	}
-
 	while ((r = read(file_from, buffer, 1024)) > 0)
 	{
-	
-		if ((w = write(file_to, buffer, r)) != r)
+
+		if (file_to < 0 || (file_to, buffer, r) != r)
 		{
-			exit(99);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
-	
+
 	}
 	if (close(file_from) < 0)
 	{
@@ -54,6 +46,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
 		exit(100);
 	}
-
 	return (0);
 }
