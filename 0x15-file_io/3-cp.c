2387079f45b173cba0f954b4008b6557f385f5e7
Islam Rafat <islam003007@gmail.com>
2023-11-11 00:01:38 +0200

    new

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - entry point.
+ *
+ * @argc: argc.
+ * @argv: argv.
+ *
+ * Return: 0.
+*/
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to, r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	r = read(file_from, buffer, 1024);
+	if (file_from < 0 || r < 0)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	w = write(file_to, buffer, r);
+	if (file_to < 0 || w < 0)
+	{
+		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+	}
+
+	if (close(file_from) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i", file_from);
+		exit(100);
+	}
+	if (close(file_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
