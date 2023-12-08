458ed804c2d36b28e1d7941a982b3b79d18e7799
islam <islam003007@gmail.com>
2023-12-08 14:09:30 +0200

    new

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
+	new->next = NULL;
+	new->prev = NULL;
 	new->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	new->next = *head;
 	(*head)->prev = new;
 	*head = new;
