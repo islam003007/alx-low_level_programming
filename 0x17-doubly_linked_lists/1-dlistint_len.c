b7d8c4dea59821aab21d432cbf83db1c4ce0f600
islam <islam003007@gmail.com>
2023-12-08 13:47:42 +0200

    new

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in dlistint_t.
+ *
+ * @h: inputs head.
+ *
+ * Return: the number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
