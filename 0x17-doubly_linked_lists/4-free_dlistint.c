c5bb589c924f71708a2ce951aaee81ed5f2b9ae1
islam <islam003007@gmail.com>
2023-12-09 06:36:38 +0200

    new

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,16 @@
 */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	do {
+		temp = head->next;
 		free(head->prev);
 		free(head);
+		head = temp;
 	} while (head->next != NULL);
 	free(head->prev);
 	free(head);
