cd4e8f535470c67be3869cd2d6e3393e557d6a08
islam <islam003007@gmail.com>
2023-12-09 08:38:05 +0200

    new

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,6 +28,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
 		*head = temp->next;
 		free(temp);
 		return (1);
