375cf11ab1074d5c7a6f2abfeef8679c5d9cce36
islam <islam003007@gmail.com>
2024-01-22 12:04:43 +0200

    new

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ *
+ * @size: inputs hash table size.
+ *
+ * Return: pointer to table. NULL in case of failure.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	return (table);
+}
