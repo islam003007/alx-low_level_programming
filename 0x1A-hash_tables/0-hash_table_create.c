106bddae26d08d55612cba1295325e1ab3e8eb75
islam <islam003007@gmail.com>
2024-01-23 10:46:20 +0200

    new

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
+
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
@@ -20,6 +22,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (table->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_set - adds an element to a hash table.
+ *
+ * @ht: inputs hash table.
+ * @key: inputs key string.
+ * @value: inputs value string.
+ *
+ * Return: 1 if it succeeded, otherwise 0.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	index = key_index((unsigned char *)key, ht->size);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+
+}
