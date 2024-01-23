2342f75a0699f4deef82b6d8c9408a5d39ed5797
islam <islam003007@gmail.com>
2024-01-23 11:00:19 +0200

    new

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (!strcmp(temp->key, key))
 		{
-			temp->value = value;
+			temp->value = strdup(value);
 			return (1);
 		}
 	}
