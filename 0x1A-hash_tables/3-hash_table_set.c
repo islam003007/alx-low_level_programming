e696fdcf1c3bf5864b4968179a154f9adbcf8092
islam <islam003007@gmail.com>
2024-01-23 11:09:45 +0200

    new

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while (temp != NULL && temp->next != NULL)
+	while (temp != NULL)
 	{
 		if (!strcmp(temp->key, key))
 		{
